24cb14283f292000cb5ef013fe5c51d989159602
Ivan Kovmir <i@kovmir.eu>
2026-08-10 12:57:13 +0200

    Minor code quality improvements
    * Check for integer overflows in read_int().
    * Add code comments.

--- stats.c
+++ stats.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <err.h>
 #include <errno.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,28 +11,38 @@
 
 #include "stats.h"
 
-int fd_read_number(int *out_buf, int fd);
+int read_int(int *out_buf, int fd);
 
 int
-fd_read_number(int *out_buf, int fd)
+read_int(int *out_buf, int fd)
 {
 	char *endp;
 	size_t n_read;
-	char buf[16];
+	char buf[32];
+	long int value;
+
 	/* Read the file. */
 	n_read = read(fd, buf, sizeof(buf)-1);
 	if (n_read <= 0)
 		return -1;
 	buf[n_read] = '\0';
+
 	/* Parse integer from the line. */
 	errno = 0;
-	*out_buf = (int)strtol(buf, &endp, 10);
+	value = strtol(buf, &endp, 10);
+
+	/* Error checking... */
+	if (value > INT_MAX || value < INT_MIN)
+		return -1; /* Integer overwlow. */
 	if (errno != 0) {
-		return -1;
+		return -1; /* Parse error. */
 	}
 	if (buf == endp) {
-		return -1;
+		return -1; /* No integer found. */
 	}
+
+	/* Good. */
+	*out_buf = value;
 	return 0;
 }
 
@@ -72,10 +83,10 @@ get_battery(BatteryInfo *bi, int cap_fd, int ac_fd)
 
 	assert(bi != NULL);
 
-	if (fd_read_number(&tmp, cap_fd) != 0)
+	if (read_int(&tmp, cap_fd) != 0)
 		return -1;
 	bi->capacity = tmp;
-	if (fd_read_number(&tmp, ac_fd) != 0)
+	if (read_int(&tmp, ac_fd) != 0)
 		return -1;
 	bi->power_state = (tmp == 1) ? POWER_AC : POWER_BATTERY;
 	return 0;
--- stats.h
+++ stats.h
@@ -2,29 +2,32 @@
  * Copyright (c) 2026 Ivan Kovmir */
 #ifndef STATS_H
 #define STATS_H
+/* Query various system stats like RAM, power state, etc. */
 
 #include <stdio.h>
 #include <time.h>
 
+/* Power sources. */
 typedef enum {
-	POWER_BATTERY,
-	POWER_AC,
+	POWER_BATTERY, /* Not charging. */
+	POWER_AC,      /* Charging. */
 } PowerState;
 
+/* Battery state. */
 typedef struct {
-	int        capacity;
+	int        capacity; /* Battery capacity: 0 to 100%. */
 	PowerState power_state;
 } BatteryInfo;
 
 /* All functions return -1 on failure, and 0 on success. */
 
-/* Return system load in /porc/loadavg fashion. */
+/* Return system load in /proc/loadavg fashion. */
 int get_load(char *out_buf, size_t out_len);
 /* Get current system date & time. */
 int get_time(char *out_buf, size_t out_len, const char *format, time_t t);
-/* Get battery charge and charging status. */
+/* Query power state. */
 int get_battery(BatteryInfo *bi, int cap_fd, int ac_fd);
-/* Get occupied RAM percentage. */
+/* Get occupied RAM: 0 to 100%. */
 int get_memory(int *out_buf, FILE *meminfo);
 
 #endif /* STATS_H */
